cbb075912050f522ec6d9b48f8bcfe54789f51fc
ivel2631 <82873290+ivel2631@users.noreply.github.com>
2021-04-30 23:04:29 +0530

    Create Example1.c

---
+++ Example1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <condo.h>
+int main(){ 
+  float radius;
+  double area;
+  clrscr();
+  printf("\n Enter the radius of the circle: ");
+  scanf("%f",%radius); area= 3.14 * radius * radius;
+  printf("\n Area = %.2lf", area); 
+  return 0;
+}
