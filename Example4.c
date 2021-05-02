0569da9024b31635fa934a81177eac2eaf066631
ivel2631 <82873290+ivel2631@users.noreply.github.com>
2021-05-02 12:36:30 +0530

    Update Example4.c

--- Example4.c
+++ Example4.c
@@ -3,5 +3,30 @@ int main(){
     char ch;
     printf("\n Enter any Character: ");
     scanf("%c",&ch);
-    switch(ch);
-    }
+    switch(ch);{
+        case ‘A’:
+        case ‘a’:
+                 printf("\n %c is VOWEL", ch);
+                 break;
+         case ‘E’:
+         case ‘e’:
+                  printf("\n %c is VOWEL", ch);
+                  break;
+         case ‘I’:
+         case ‘i’:
+                    printf("\n %c is VOWEL", ch);
+                    break;
+         case ‘O’:
+         case ‘o’:
+                     printf("\n %c is VOWEL", ch);
+                     break;
+         case ‘U’:
+         case ‘u’:
+                     printf("\n %c is VOWEL", ch);
+                     break;
+    
+ default: printf("\n %c is not a vowel", ch);
+}
+return 0;
+}
+
